a8c5ec6b300b9283f100fdbc7f9fad77115194dc
Sam Poate <sampoate@outlook.com>
2020-02-22 18:46:13 +0000

    General Updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,27 @@
 import '../scss/main.scss';
+import _ from 'lodash';
 
-const banana = () => {
-    let f = 'Hello';
+const navbar = document.getElementById('navbar');
 
-    console.log(f);
+const onScroll = () => {
+    const scroll = document.documentElement.scrollTop;
+
+    if (scroll > 0) {
+        navbar.classList.add('bg--white');
+    } else {
+        navbar.classList.remove('bg--white');
+    }
 };
+
+const scrollToPartnerships = () => {
+    document.getElementById('sponsorship').scrollIntoView({
+        behavior: 'smooth'
+    });
+};
+
+const throttledOnScroll = _.throttle(onScroll, 100, {});
+
+window.addEventListener('scroll', throttledOnScroll);
+document
+    .getElementById('more-arrows')
+    .addEventListener('click', scrollToPartnerships);
